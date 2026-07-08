2e8f9f90a09e43ee8787a5716286b6a43fe0f101
Yago Elias <yagoelias024@gmail.com>
2026-07-08 01:12:15 -0300

    fix: adicionar a extensão pdo_sqlite e corrigir cominho da base

--- app/models/Connection.php
+++ app/models/Connection.php
@@ -10,7 +10,7 @@ class Connection {
         // $pdo = new PDO("mysql:host=localhost;dbname=users", "root", "root");
         // $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-        $pdo = new PDO("sqlite:database/database.sqlite");
+        $pdo = new PDO("sqlite:" . __DIR__ . "/../../database/database.sqlite");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
